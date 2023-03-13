423f96d4fcdf75bdd85e3ab3d0fcf77027c3081d
Filippo Marinolli <f.marinolli@gmail.com>
2023-03-13 03:21:42 +0100

    projectile system working

--- js/index.js
+++ js/index.js
@@ -1,59 +1,55 @@
 const myCanvas = document.getElementById("canvas");
 const ctx = myCanvas.getContext("2d");
 
-import Map from "./map.js";
+import Map from "./map.js"; //importing files
 import Ship from "./ship.js";
+import Projectile from "./projectiles.js";
 
-const ship = new Ship(ctx,"./images/fighter.png" )
+const ship = new Ship(ctx, "./images/fighter.png"); //creating the object for the game
+const riverOne = new Map(ctx, "./images/river.jpg", 0);
+const riverTwo = new Map(ctx, "./images/river.jpg", -700);
 
-
-const riverOne = new Map(ctx, "./images/river.jpg", 0)
-const riverTwo = new Map(ctx, "./images/river.jpg", -700)
-
-
-document.addEventListener("mousemove", getMousePos, false);
-
-myCanvas.style.cursor = "none";
+myCanvas.style.cursor = "none"; //deactivating right mouse click on canvas
 myCanvas.oncontextmenu = function (e) {
   e.preventDefault();
 };
 
-function getMousePos( evt) {
+document.addEventListener("mousemove", getMousePos, false); //mouse over position to ship
+function getMousePos(evt) {
   var rect = myCanvas.getBoundingClientRect();
-  console.log(evt)
-  ship.x= evt.clientX - rect.left -25
-  ship.y=evt.clientY- rect.top -35
+  console.log(evt);
+  ship.x = evt.clientX - rect.left - 25;
+  ship.y = evt.clientY - rect.top - 35;
 }
 
-
-let carImg = new Image();
-carImg.src = "./images/fighter.png";
+document.addEventListener("mousedown", (key) => {
+  //click functions
+  switch (key.button) {
+    case 0:
+      shotRocket();
+      side++
+      break;
+    case 2:
+      console.log("right click");
+  }
+});
 
 function getRandom(min, max) {
+  // auxilliary random generation function with min and max
   return Math.random() * (max - min) + min;
 }
 
-/* class blocksC {
-  constructor() {
-    this.x = 30+getRandom(10,400);
-    this.y = 0;
-    this.widht = getRandom(50,200);
-    this.height = getRandom(10, 80);
-  }
+const myShots = [];
 
-  draw() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(this.x, this.y, this.widht, this.height);
-  }
+let side=0
 
-  move() {
-    this.y += 1;
-  }
-} */
+function shotRocket() {
+  if (side%2===0) myShots.push(new Projectile(ctx, "./images/pro.png", ship.x+27, ship.y));
+  else myShots.push(new Projectile(ctx, "./images/pro.png", ship.x, ship.y));
+}
 
-let score=0 
+let score = 0;
 let c = 0;
-
 
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
@@ -63,30 +59,24 @@ window.onload = () => {
 
 let counter = 0;
 
-document.addEventListener("mousedown", (key) => {
-  switch (key.button) {
-    case 0:
-      console.log("left click")
-      break
-    case 2:
-      console.log("right click")
-  }
-});
-
 function startGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   const interval = setInterval(() => {
     counter++;
-    riverOne.draw()
-    riverOne.scroll()
-    riverTwo.draw()
-    riverTwo.scroll()
-    if(riverOne.y==700)riverOne.y=-700
-    if(riverTwo.y==700)riverTwo.y=-700
+    riverOne.draw();
+    riverOne.scroll();
+    riverTwo.draw();
+    riverTwo.scroll();
+    if (riverOne.y == 700) riverOne.y = -700;
+    if (riverTwo.y == 700) riverTwo.y = -700;
 
-    ship.draw()
-    
-    
+    ship.draw();
+
+     for (const shot of myShots){
+      shot.move();
+      shot.draw();
+    } 
+
     /* if (Math.random() * 1000 <= level) {
       blocks.push(new blocksC(100, 50));
     }
@@ -122,9 +112,9 @@ function startGame() {
 
     } */
     c++;
-    ctx.fillStyle = 'orange';
-    ctx.font = '40px Arial';  
-    ctx.strokeText("Your score is " +score, 150, 50)
+    ctx.fillStyle = "orange";
+    ctx.font = "40px Arial";
+    ctx.strokeText("Your score is " + score, 150, 50);
 
     let x = Math.floor(Math.random() * 100 + 1);
 
@@ -132,17 +122,14 @@ function startGame() {
   }, 1000 / 60);
 }
 
-function gameOver(interval){
-  ctx.fillStyle ='black'
-  ctx.fillRect(0,0 , 500, 700)
-  ctx.fillStyle ='purple'
-  
+function gameOver(interval) {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 500, 700);
+  ctx.fillStyle = "purple";
 
-  ctx.fillText("You failed as usual!!", 50, 350)
-  clearInterval(interval)
-  blocks=[1,2,3]
-
-
+  ctx.fillText("You failed as usual!!", 50, 350);
+  clearInterval(interval);
+  blocks = [1, 2, 3];
 }
 
 /* 
---
+++ js/projectiles.js
@@ -0,0 +1,23 @@
+class Projectile{
+    constructor(ctx, imgSrc,x ,y  ){
+        this.ctx= ctx
+        this.x=x;
+        this.y= y;
+        this.img= new Image;
+        this.img.src=imgSrc;
+
+    }
+
+
+    draw() {
+        this.ctx.drawImage(this.img, this.x, this.y, 20, 20);
+      }
+
+    move(){
+        this.y-=2
+    } 
+}
+
+
+
+export default Projectile
